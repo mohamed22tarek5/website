ff04c0ed3680041e605333e76d3e3f535341a2c3
Mohamed Tarek <mohammed.tarek.abdelhady.ali@gmail.com>
2026-09-19 23:48:21 +0300

    1
    1

--- public/pwa.js
+++ public/pwa.js
@@ -227,7 +227,7 @@ class PWAManager {
     this.installButton = document.createElement('button');
     this.installButton.id = 'installButton';
     this.installButton.className = 'pwa-install-btn';
-    this.installButton.innerHTML = '<i class="fas fa-download"></i> Install App';
+    this.installButton.innerHTML = '<svg class="fas fa-download" style="width:1em;height:1em;vertical-align:-.125em;fill:currentColor" viewBox="0 0 512 512" aria-hidden="true" focusable="false"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg> Install App';
     this.installButton.setAttribute('aria-label', 'Install application');
     document.body.appendChild(this.installButton);
 
